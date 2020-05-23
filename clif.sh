b79707a3b1c653ff3460fffcfa472013c2b4e539
Yijun Yu <y.yu@open.ac.uk>
2020-05-23 09:35:07 +0000

    update

--- clif.sh
+++ clif.sh
@@ -115,10 +115,7 @@ ln -s -f -n "$CLIFSRC_DIR/clif" clif
 git clone https://github.com/python/cpython /workspace/cpython && cd !$ && git checkout v3.8.2 
 ./configure --prefix=/home/gitpod/.pyenv/versions/3.8.2 --enable-shared && make -j16 && make install
 cp /workspace/cpython/libpython3.8.a /home/gitpod/.pyenv/versions/3.8.2/lib/libpython3.8.a
-sed -i -e 's/Expected<std::vector<char>> readMem/Expected<std::vector<uint8_t>> readMem/g' workspace/clif_backend/llvm/llvm/include/llvm/ExecutionEngine/Orc/OrcRemoteTargetClient.h
-sed -i -e 's/PyString_AS_STRING/PyBytes_AS_STRING/g' /workspace/clif_backend/llvm/build_matcher/tools/clif/python/utils/proto_util.cc
-sed -i -e 's/Py_TPFLAGS_CHECKTYPES/Py_TPFLAGS_HEAPTYPE/g' /workspace/clif_backend/llvm/build_matcher/tools/clif/python/utils/proto_util.cc
-
+cp /workspace/fastseq/proto_util.cc /workspace/clif_backend/llvm/build_matcher/tools/clif/python/utils/proto_util.cc
 mkdir -p "$BUILD_DIR"
 cd "$BUILD_DIR"
 cmake -DCMAKE_INSTALL_PREFIX="$CLIF_VIRTUALENV/clang" \
