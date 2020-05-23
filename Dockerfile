de1522bc5076a5c097cf1c1c2fb6dd215ba4d581
Yijun Yu <y.yu@open.ac.uk>
2020-05-23 09:17:57 +0000

    update

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,7 @@ RUN apt-get update -y
 RUN apt-get install -y subversion
 RUN apt-get install -y llvm clang
 RUN apt-get install -y python3-dev python3-pip python3-tk python3-lxml python3-six
-RUN apt-get install -y python-dev python-pip
+RUN apt-get install -y python-dev python-pip libpython2.7-dev
 RUN apt-get install -y ocaml opam
 RUN apt-get install -y graphviz
 RUN apt-get install -y texlive
--- clif.sh
+++ clif.sh
@@ -116,6 +116,7 @@ git clone https://github.com/python/cpython /workspace/cpython && cd !$ && git c
 ./configure --prefix=/home/gitpod/.pyenv/versions/3.8.2 --enable-shared && make -j16 && make install
 cp /workspace/cpython/libpython3.8.a /home/gitpod/.pyenv/versions/3.8.2/lib/libpython3.8.a
 sed -i -e 's/Expected<std::vector<char>> readMem/Expected<std::vector<uint8_t>> readMem/g' workspace/clif_backend/llvm/llvm/include/llvm/ExecutionEngine/Orc/OrcRemoteTargetClient.h
+sed -i -e 's/PyString_AS_STRING/PyBytes_AS_STRING/g' /workspace/clif_backend/llvm/build_matcher/tools/clif/python/utils/proto_util.cc
 
 mkdir -p "$BUILD_DIR"
 cd "$BUILD_DIR"
