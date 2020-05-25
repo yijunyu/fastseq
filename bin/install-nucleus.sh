f06dba99da9bdb53a1d9ab6029870217ed4d2540
Yijun Yu <y.yu@open.ac.uk>
2020-05-25 23:38:18 +0000

    *

--- bin/install-nucleus.sh
+++ bin/install-nucleus.sh
@@ -1,6 +1,6 @@
 cd /workspace \
  && git clone https://github.com/google/nucleus
-ENV CLIF_FLAGS "--copt=-msse4.1 --copt=-msse4.2 --copt=-mavx --copt=-O3"
+export CLIF_FLAGS="--copt=-msse4.1 --copt=-msse4.2 --copt=-mavx --copt=-O3"
 cd /workspace/nucleus \
   && bazel build -c opt ${CLIF_FLAGS} nucleus/...
 cd /workspace/nucleus \
