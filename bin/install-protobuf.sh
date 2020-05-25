437222483fd7b0a0458363a18bb06ce55029ce66
Yijun Yu <y.yu@open.ac.uk>
2020-05-25 21:22:43 +0000

    *

--- bin/install-protobuf.sh
+++ bin/install-protobuf.sh
@@ -1,8 +1,12 @@
 command -v protoc >/dev/null 2>&1 || {
-	if [ -f bin/xaa ]; then
-		mkdir -p /workspace/protobuf
-		cat bin/xa* | tar xvfj - -C /workspace/protobuf
-	fi
-	echo export PATH='"/workspace/protobuf/bin:$PATH"' >> .bashrc
-	source .bashrc
+	git clone https://github.com/google/protobuf
+	cd protobuf
+	git checkout v3.6.0
+	./autogen.sh
+	./configure --prefix=/workplace/fastseq/nucleus-0.5.1/usr/local
+	make -j 16
+	make install
+	grep "nucleus-0.5.1/usr/local/bin" .bashrc > /dev/null 2>& 1 || {
+		echo export PATH='"/workspace/fastseq/nucleus-0.5.1/usr/local/bin:$PATH"' >> .bashrc
+	}
 }
