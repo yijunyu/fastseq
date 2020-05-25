1708f6b7a9a3656fd2f8cbc7655a40df31c199ae
Yijun Yu <y.yu@open.ac.uk>
2020-05-25 09:45:10 +0000

    *

--- bin/install-nucleus.sh
+++ bin/install-nucleus.sh
@@ -1,19 +1,15 @@
-command -v nucleus >/dev/null 2>&1 || {
+if [ ! -f nucleus-0.5.1/bazel-bin/licenses.zip ]; then
 	bin/install-protobuf.sh
 	bin/install-clif.sh
 	bin/install-pyclif.sh
-	if [ ! -f bin/nucleus.tar.bz2 ]; then
+	if [ -f bin/nucleusaa ]; then
 		if [ ! -d nucleus-0.5.1 ]; then
 			wget https://github.com/google/nucleus/archive/0.5.1.tar.gz
 			tar xvfz 0.5.1.tar.gz
 		fi
-		cd nucleus-0.5.1
-        patch install.sh < /workspace/fastseq/bin/install.sh.patch
-		./install.sh 
-		cd /workspace && tar cvfj fastseq/bin/nucleus.tar.bz2 usr && cd -
-	else
-		tar xvfj bin/nucleus.tar.bz2 -C /workspace
+		cat bin/nucleusa* | tar xvfj - -C nucleus-0.5.1
 	fi
-	echo export PATH='"/workspace/.pipmodules/bin:$PATH"' >> .bashrc
-	. .bashrc
-}
+	echo export PATH='"/workspace/fastseq/nucleus-0.5.1/usr/local/clif/bin:$PATH"' >> .bashrc
+	echo export LD_LIBRARY_PATH='"/workspace/fastseq/nucleus-0.5.1/usr/local/lib"' >> .bashrc
+fi
+cd nucleus-0.5.1
