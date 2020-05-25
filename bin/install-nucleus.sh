4cb5437ffa5e544da0bf78af6699fa5ebb187dac
Yijun Yu <y.yu@open.ac.uk>
2020-05-25 08:41:08 +0000

    *

---
+++ bin/install-nucleus.sh
@@ -0,0 +1,18 @@
+command -v nucleus >/dev/null 2>&1 || {
+	bin/install-protobuf.sh
+	bin/install-clif.sh
+	bin/install-pyclif.sh
+	if [ ! -f bin/nucleus.tar.bz2 ]; then
+		if [ ! -d nucleus-0.5.1 ]; then
+			wget https://github.com/google/nucleus/archive/0.5.1.tar.gz
+			tar xvfz 0.5.1.tar.gz
+		fi
+		cd nucleus-0.5.1
+		./install.sh 
+		cd /workspace && tar cvfj fastseq/bin/nucleus.tar.bz2 .pipmodules && cd -
+	else
+		tar xvfj bin/nucleus.tar.bz2 -C /workspace
+	fi
+	echo export PATH='"/workspace/.pipmodules/bin:$PATH"' >> .bashrc
+	. .bashrc
+}
