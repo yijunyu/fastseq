9c814eb80c04ca104116eceedc4d812ea0d911bd
Yijun Yu <y.yu@open.ac.uk>
2020-05-26 12:32:12 +0000

    *

--- bin/install-nucleus.sh
+++ bin/install-nucleus.sh
@@ -1,11 +1,11 @@
 export CLIF_FLAGS="--copt=-msse4.1 --copt=-msse4.2 --copt=-mavx --copt=-O3"
 cd /workspace \
- && git clone https://github.com/tensorflow/tensorflow \
+ && if [ ! -d tensorflow ]; then git clone https://github.com/tensorflow/tensorflow; fi \
  && cd tensorflow \
  && git checkout v2.0.0 \
  && echo | ./configure \
  && cd .. \
- && git clone https://github.com/google/nucleus \
+ && if [ ! -d nucleus ]; then git clone https://github.com/google/nucleus; fi \
  && cd nucleus \
  && bazel build -c opt ${CLIF_FLAGS} nucleus/... \
  && bazel build :licenses_zip
